6524b81336dd53cdc17681b7c49de865d7d150c6
alexandrecasgrainouimet <alexandrecasgrainouimet@github.com>
2022-03-15 21:11:16 -0400

    restricting data type for meteological data

--- apps/usingOurMeteorologicalDataset.py
+++ apps/usingOurMeteorologicalDataset.py
@@ -183,7 +183,7 @@ def parse_contents_fromInteractiveDT(all_rows_data):
         dcc.Dropdown(id='col-specimens', options=[{'label':x, 'value':x} for x in dff.columns]),
         html.H4("select the name of the column to analyze"),
         dcc.Markdown('The values of the column **must be numeric** for the program to work properly.'),
-        dcc.Checklist(id = 'col-analyze', options =[{'label': x, 'value': x} for x in dff.columns],
+        dcc.Checklist(id = 'col-analyze', options =[{'label': x, 'value': x} for x in dff._get_numeric_data().columns],
                         labelStyle={'display': 'inline-block', 'marginRight':'20px'}),
         html.Br(),
         html.Button(id="submit-forTree", children="Create Newick files"),  
