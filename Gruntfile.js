f0b08b512b6f2b69735e23f1b4650fe40126b741
lucavadnais <74997511+lucavadnais@users.noreply.github.com>
2023-03-20 12:40:25 -0400

    Css refactoring

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,13 +7,27 @@ module.exports = function (grunt) {
             },
             dist: {
                 files: {
-                    'assets/main.css': 'assets/scss/main.scss'
+                    'assets/main.css': 'assets/styles/main.scss',
+                    'assets/home_page.css': 'assets/styles/home_page.scss',
+                    'assets/nav_bar.css': 'assets/styles/nav_bar.scss',
+                    'assets/results.css': 'assets/styles/results.scss',
+                    'assets/upload_page.css': 'assets/styles/upload_page.scss',
+                    'assets/helper.css': 'assets/styles/utils/helper.scss',
+                    'assets/button.css': 'assets/styles/utils/button.scss'
                 }
             }
         },
         watch: {
             sass: {
-                files: ['assets/scss/main.scss'],
+                files: [
+                    'assets/styles/main.scss',
+                    'assets/styles/home_page.scss',
+                    'assets/styles/nav_bar.scss',
+                    'assets/styles/results.scss',
+                    'assets/styles/upload_page.scss',
+                    'assets/styles/utils/helper.scss',
+                    'assets/styles/utils/button.scss',
+                ],
                 tasks: ['build-css']
             }
         }
