a2e2a3a5c772804cc4388ecccdd366e7b44800bd
jsDesm <77494060+jsDesm@users.noreply.github.com>
2023-04-17 15:17:39 -0400

    Final modification to readme and popup message

--- apps/pages/utils/popup.py
+++ apps/pages/utils/popup.py
@@ -7,7 +7,7 @@ layout = html.Div([
                 html.Div([
                     html.Img(src='../../assets/icons/close.svg', className="icon", id="close_popup"),
                 ], className="close"),
-                html.Div('Please don\'t close your browser window until we finish generating your file', className="title"),
+                html.Div('Your results are on the way!', className="title"),
                 html.A([
                     html.Img(src='../../assets/img/coffee-cup.gif', className="icon"),
                 ], href="https://www.flaticon.com/authors/freepik", target="_blank", className="link"),
